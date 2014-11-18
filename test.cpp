dfc5dca569f9ebf0f1ba3ec93a76edb8cf17688a
Peter Stuifzand <peter@stuifzand.eu>
2014-11-18 15:08:58 +0100

    Refactor main function of test program

--- test.cpp
+++ test.cpp
@@ -3,26 +3,28 @@
 #include <iostream>
 #include "mysql.hpp"
 
-int main()
-{
-    mysql sql{};
-
-    sql.connect("localhost", "test", "test", "test", 0, 0, 0);
-
-    {
-        Stmt stmt = sql.prepare("DELETE FROM `test` WHERE `id` = ?");
-        std::string s;
-        while (std::cin) {
-            getline(std::cin, s);
-            if (s.size() == 0) break;
-            stmt.execute(s);
-        }
-    }
-
+void fetch_from_test(mysql& sql) {
     Result r = sql.query("SELECT `id`, `name` FROM `test`");
-
     while (Row row = r.fetch_row()) {
         std::cout << std::stoi(row[0]) << "\t" << row[1] << "\n";
     }
+}
+
+void delete_from_test(mysql& sql)
+{
+    Stmt stmt = sql.prepare("DELETE FROM `test` WHERE `id` = ?");
+    std::string s;
+    while (getline(std::cin, s)) {
+        stmt.execute(s);
+    }
+}
+
+int main()
+{
+    mysql sql{};
+    sql.connect("localhost", "test", "test", "test", 0, 0, 0);
+
+    delete_from_test(sql);
+    fetch_from_test(sql);
 }
 
